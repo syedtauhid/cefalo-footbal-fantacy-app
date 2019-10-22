6095f630a653f50325ab67fcd7d1dba408175468
Miraz <miraz.freedom@gmail.com>
2019-10-22 11:26:31 +0600

    list with data

--- src/components/Players/PlayersList.js
+++ src/components/Players/PlayersList.js
@@ -4,6 +4,10 @@ import PlayerRow from './PlayerRow'
 import Loader from '../Loader'
 
 const PlayersList = () => {
+    const data = [
+        {player: 'Player 1', image: 'https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/40x40/Photo-Missing.png', position: 'Forward', flag: 'TR', nationality: 'Turkey'},
+        {player: 'Rolando Aarons', image: 'https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/40x40/p155513.png', position: 'Midfielder', flag: 'GB-ENG', nationality: 'England'}
+    ]
     return (
         <>
         <div className="wrapper">
@@ -18,8 +22,7 @@ const PlayersList = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <PlayerRow data={{player: 'Player 1', image: 'https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/40x40/Photo-Missing.png', position: 'Forward', flag: 'TR', nationality: 'Turkey'}}/>
-                        <PlayerRow data={{player: 'Rolando Aarons', image: 'https://premierleague-static-files.s3.amazonaws.com/premierleague/photos/players/40x40/p155513.png', position: 'Midfielder', flag: 'GB-ENG', nationality: 'England'}}/>
+                        { data.map((d, i) => <PlayerRow key={i} data={d} />) }
                     </tbody>
                 </table>
             </div>
